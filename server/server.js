8492c28fe21e9b27246ffd891bd6ce40db8915c1
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-07-02 14:23:43 +0300

    Creating connection to sql

--- server/server.js
+++ server/server.js
@@ -1,5 +1,22 @@
 const express = require('express')
 const app = express()
+const mysql = require('mysql')
+
+const database = mysql.createConnection({
+    user: 'root',
+    host: 'localhost',
+    password: '',
+    database: 'crud_app_test'
+})
+
+app.post('/create', (req, res) => {
+    const name = req.body.name
+    const surname = req.body.surname
+    const date_of_birth = req.body.date_of_birth
+    const coolness = req.body.coolness
+    const phone = req.body.phone
+    const email = req.body.email
+})
 
 
 app.listen(3001, () => {
