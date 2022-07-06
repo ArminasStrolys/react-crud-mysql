ac3b43ca0ed0a3908027fe24a7c4f3c920cea9bd
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-07-06 16:21:18 +0300

    Update Users.jsx

--- client/src/components/users/Users.jsx
+++ client/src/components/users/Users.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Users = (props) => {
     return (
         <div className='user_bar'>
-            <p>{props.name} {props.surname}</p>
+            <p><b>{props.name} {props.surname}</b></p>
             <p>DOB: {props.dob.slice(0,10)}. Coolness lvl: {props.cool}</p>
             <p>Phone: +37{props.phone}. E-mail: {props.email}</p>
         </div>
